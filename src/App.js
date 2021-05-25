82aa75e9ecb535624ac21a47f293ab854b1bfb48
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-05-25 11:54:27 +0200

    create form html/css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,64 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Maak hier jouw formulier!
+
+    <div className="body">
+
+    <form>
+      <fieldset>
+        <legend>Gegevens</legend>
+      <label>Naam : </label>
+      <input
+      type="tekst"
+      name="Naam"
+
+      />
+      <p/>
+      <label>Leeftijd : </label>
+      <input
+          type="tekst"
+          name="Leeftijd"
+      />
+      </fieldset>
+    </form>
+<p>
+
+
+</p>
+      <form>
+        <fieldset>
+          <legend>Jouw review</legend>
+          <label>Hoe heb je dit recept gevonden ? </label>
+          <br/>
+          <br/>
+          <select>
+            <option value="google">Google</option>
+            <option value="vriend">Vriend</option>
+            <option value="anders">Anders</option>
+          </select>
+
+
+          <br/>
+          <br/>
+          <p/>
+          <label>Opmerking : </label>
+          <br/>
+          <br/>
+          <input
+              type="tekst"
+              name="veld"
+              placeholder="Wat vond je van het recept ?"
+              id="invoer"
+
+          />
+          <br/>
+          <br/>
+          <button>Versturen</button>
+        </fieldset>
+      </form>
     </div>
+
+
   );
 }
 
